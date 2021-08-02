d58cc81939aab89afd92c4ba289ad0c94d106f47
John Garcia <john.garcia@ajila.com>
2021-08-02 23:08:24 +0200

    init commit

---
+++ src/main/java/com/gwendolinanna/ws/auth/app/restassuredtest/GwendolinannaWsAuthRestAssuredTestApplication.java
@@ -0,0 +1,13 @@
+package com.gwendolinanna.ws.auth.app.restassuredtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwendolinannaWsAuthRestAssuredTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwendolinannaWsAuthRestAssuredTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gwendolinanna/ws/auth/app/restassuredtest/GwendolinannaWsAuthRestAssuredTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwendolinanna.ws.auth.app.restassuredtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwendolinannaWsAuthRestAssuredTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
